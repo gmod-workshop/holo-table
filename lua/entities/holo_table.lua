a90ed54b7c5de1c12e0f595aece0e459b6a0f578
David Katz <dhkatz.16@gmail.com>
2019-09-30 00:08:30 -0700

    Add bottom plane clipping and fix rotation issue

--- lua/entities/holo_table.lua
+++ lua/entities/holo_table.lua
@@ -26,6 +26,8 @@ function ENT:Initialize()
 
         self.Mask = Mesh()
         self.Mask:BuildFromTriangles(meshes[1].triangles)
+
+        self.Ghosts = {}
     end
 end
 
@@ -35,6 +37,39 @@ function ENT:SetupDataTables()
 
     self:NetworkVar('Float', 0, 'Height', { KeyName = 'height', Edit = { type = 'Float', min = -500, max = 500 }})
     self:NetworkVar('Float', 1, 'Scale', { KeyName = 'scale', Edit = { type = 'Float', min = 1, max = 500 } })
+end
+
+function ENT:Think()
+    if SERVER then return end
+
+    local ghosts = {}
+    for k, v in pairs(self.Ghosts) do
+        if IsValid(v.ent) then
+            ghosts[v.ent:EntIndex()] = v
+        else
+            SafeRemoveEntity(v.csent)
+        end
+    end
+
+    for k, v in ipairs(ents.GetAll()) do
+        if not (v.LFS or v.IsSWVehicle or v.IsSWVRVehicle) then continue end
+
+        if ghosts[v:EntIndex()] and IsValid(ghosts[v:EntIndex()].csent) then continue end
+
+        local ent = ents.CreateClientProp(v:GetModel())
+
+        ghosts[v:EntIndex()] = { ent = v, csent = ent }
+    end
+
+    self.Ghosts = ghosts
+end
+
+function ENT:OnRemove()
+    if SERVER then return end
+
+    for k, v in ipairs(self.Ghosts) do
+        SafeRemoveEntity(v.csent)
+    end
 end
 
 function ENT:Draw()
@@ -63,7 +98,7 @@ function ENT:Draw()
     local size = 90
     local segments = 32
 
-    local pos = self:GetPos() + Vector(0, 0, 27)
+    local pos = self:GetPos() + self:GetUp() * 26
     local up = self:GetAngles():Right()
     local right = self:GetAngles():Forward()
 
@@ -97,10 +132,14 @@ function ENT:Draw()
     render.SetStencilCompareFunction( STENCIL_EQUAL ) --Only draw if pixel value == reference value
     render.ClearBuffersObeyStencil( 0, 0, 0, 0, true )
 
+    -- Use a clip plane to stop anything below the plane of the table from being rendered
+    local clip = render.EnableClipping(true)
+    render.PushCustomClipPlane(self:GetUp(), self:GetUp():Dot(self:GetPos() + Vector(0, 0, 25)))
+
     local m2 = Matrix()
-    m2:Translate(self:GetPos() + Vector(0, 0, self:GetHeight()))
+    m2:SetTranslation(self:GetPos() + self:GetUp() * self:GetHeight())
     m2:SetAngles(self:GetAngles())
-    m2:SetScale(Vector(1 / scale, 1 / scale, 1 / scale))
+    m2:Scale(Vector(1 / scale, 1 / scale, 1 / scale))
 
     render.PushFilterMag(TEXFILTER.ANISOTROPIC)
     render.PushFilterMin(TEXFILTER.ANISOTROPIC)
@@ -114,25 +153,26 @@ function ENT:Draw()
             end
         end
 
-        for k, v in ipairs(player.GetAll()) do
-            render.SetColorMaterial()
-            render.DrawSphere(v:GetPos(), scale, 10, 10, Color(255, 0, 0))
-        end
-
-        for k, v in ipairs(ents.GetAll()) do
-            if v:IsNPC() then
-                render.SetColorMaterial()
-                render.DrawSphere(v:GetPos(), scale, 10, 10, Color(0, 0, 255))
-            elseif v.LFS or v.IsSWVehicle or v.IsSWVRVehicle then
-                render.SetColorMaterial()
-                render.DrawSphere(v:GetPos(), scale, 10, 10, Color(0, 255, 255))
-            end
-        end
-
         if self:GetEntities() then
             for k, v in ipairs(map.entities) do
                 v:DrawModel()
             end
+        end
+
+        for k, v in pairs(self.Ghosts) do
+            v.csent:SetPos(v.ent:GetPos())
+            v.csent:SetAngles(v.ent:GetAngles())
+            v.csent:SetModelScale(1 / scale, 0)
+            v.csent:DrawModel()
+
+            render.SetColorMaterial()
+            -- render.SetColorModulation( 255, 0, 0 )
+            render.DrawSphere(v.ent:GetPos(), scale, 10, 10, Color(255, 0, 0, 255))
+        end
+
+        for k, v in ipairs(player.GetAll()) do
+            render.SetColorMaterial()
+            render.DrawSphere(v:GetPos(), scale, 10, 10, Color(255, 0, 0))
         end
     cam.PopModelMatrix()
 
@@ -141,6 +181,9 @@ function ENT:Draw()
     render.PopFilterMag()
 
     render.SetStencilEnable(false)
+
+    render.PopCustomClipPlane()
+    render.EnableClipping(clip)
 
     self:DrawModel()
 end
